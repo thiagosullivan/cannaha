96c8f2f41bc49f570efc530167b298234b78d35b
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-08-10 03:18:51 -0300

    Highlight section

---
+++ components/HighlightHome/HighlightHome.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ProductCard from '../ProductCard/ProductCard';
+import ProductImg from '../../assets/product-img.png';
+import { HighlightHomeContainer } from './style';
+import Link from 'next/link';
+
+function HighlightHome() {
+  return (
+    <HighlightHomeContainer>
+        <h2>Destaques</h2>
+        <div className='highlight__prod__container'>
+            <ProductCard
+                productName="CBD Oil 2.75% - 10 ml - 275 mg cannabidiol"
+                productDesc="Fusce ut fringilla quam. Pellentesque tempor placerat egestas. Suspendisse pharetra dui a fermentum eleifend"
+                productImage={ProductImg}
+                producUrl="/"
+            />
+            <ProductCard
+                productName="CBD Oil 2.75% - 10 ml - 275 mg cannabidiol"
+                productDesc="Fusce ut fringilla quam. Pellentesque tempor placerat egestas. Suspendisse pharetra dui a fermentum eleifend"
+                productImage={ProductImg}
+                producUrl="/"
+            />
+            <ProductCard
+                productName="CBD Oil 2.75% - 10 ml - 275 mg cannabidiol"
+                productDesc="Fusce ut fringilla quam. Pellentesque tempor placerat egestas. Suspendisse pharetra dui a fermentum eleifend"
+                productImage={ProductImg}
+                producUrl="/"
+            />
+            <ProductCard
+                productName="CBD Oil 2.75% - 10 ml - 275 mg cannabidiol"
+                productDesc="Fusce ut fringilla quam. Pellentesque tempor placerat egestas. Suspendisse pharetra dui a fermentum eleifend"
+                productImage={ProductImg}
+                producUrl="/"
+            />
+            <ProductCard
+                productName="CBD Oil 2.75% - 10 ml - 275 mg cannabidiol"
+                productDesc="Fusce ut fringilla quam. Pellentesque tempor placerat egestas. Suspendisse pharetra dui a fermentum eleifend"
+                productImage={ProductImg}
+                producUrl="/"
+            />
+            <ProductCard
+                productName="CBD Oil 2.75% - 10 ml - 275 mg cannabidiol"
+                productDesc="Fusce ut fringilla quam. Pellentesque tempor placerat egestas. Suspendisse pharetra dui a fermentum eleifend"
+                productImage={ProductImg}
+                producUrl="/"
+            />
+        </div>
+
+        <div className='seemore__btn'>
+            <Link href="/">Veja mais</Link>
+        </div>
+    </HighlightHomeContainer>
+  )
+}
+
+export default HighlightHome
---
+++ components/HighlightHome/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const HighlightHomeContainer = styled.section`
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+
+    h2 {
+        font-size: 4rem;
+        color: ${({theme}) => theme.primary};
+        font-family: 'Prompt', sans-serif;
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 3rem;
+    }
+
+    .highlight__prod__container {
+        width: 100%;
+        align-items: center;
+        margin-bottom:  3.4rem;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(290px, 315px));
+        justify-content: center;
+        gap: 1rem;
+    }
+`;
