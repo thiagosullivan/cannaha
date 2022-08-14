3d6ae8560de813388c2cc96cc85af094be0d38d2
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-08-14 00:50:52 -0300

    Adding slug page for products

---
+++ pages/produtos/[slug].js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
+import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
+import ProductGallery from '../../components/ProductGallery/ProductGallery';
+import ProductPageInfos from '../../components/ProductPageInfos/ProductPageInfos';
+
+function ProductPage(){
+    return (
+        <div id="product__page">
+            <Head>
+                <title>Produto X | Cannaha CBD</title>
+                <meta name="description" content="Cannaha" />
+                {/* <meta name="title" content={`${post.title} | Cannaha CBD`}/> */}
+            </Head>
+            <Header />
+            <main>
+                <Breadcrumb />
+                <ProductPageInfos />
+            </main>
+            <Footer />
+            <ScrollToTop />
+        </div>
+    )
+};
+
+export default ProductPage;
